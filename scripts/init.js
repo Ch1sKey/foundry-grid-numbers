53c60aa856200683d968a2305dbbb1f87938acc0
Kirill Chistiakov <kirill77785@gmail.com>
2022-04-26 00:33:00 +0400

    Beta raedy

--- scripts/init.js
+++ scripts/init.js
@@ -1,47 +1,44 @@
-console.log("Hello World! This code runs immediately when the file is loaded.");
-const logger = (...rest) => {
-  console.log('MODULE: ', ...rest);
-}
-
-Hooks.on("init", function() {
-  logger(`
-  --------------
-    This code runs once the Foundry VTT software begins its initialization workflow.
-  --------------`);
-});
-
-
-const FALLBACK_TEXT_STYLE = {
-  fontFamily : 'Arial',
-  fontSize: 9,
-  fill : 0xff1010,
-  align : 'center',
-}
-
-// Agressive monkey patching
-GridLayer.prototype._draw = GridLayer.prototype.draw;
-GridLayer.prototype.draw = async function() {
+function patch() {
+  const FALLBACK_TEXT_STYLE = {
+    fontFamily: 'Arial',
+    fontSize: 9,
+    fill: 0xff1010,
+    align: 'center',
+  }
+  
+  // Agressive monkey patching
+  GridLayer.prototype._draw = GridLayer.prototype.draw;
+  GridLayer.prototype.__GRID_NUMBERS_MODULE__showNumbers = true;
+  GridLayer.prototype.__GRID_NUMBERS_MODULE__setShowNumbers = function (status) {
+    this.__GRID_NUMBERS_MODULE__showNumbers = status;
+    this.draw();
+  }
+  GridLayer.prototype.__GRID_NUMBERS_MODULE__toggleShowNumbers = function () {
+    this.__GRID_NUMBERS_MODULE__setShowNumbers(!this.__GRID_NUMBERS_MODULE__showNumbers)
+  }
+  
+  GridLayer.prototype.draw = async function () {
     await this._draw();
-    if(!this.grid) {
+    if (!this.grid) {
       console.warn(`MODULE: GRID-NUMBERS: No grid yet Can't draw numbers`);
       return;
     };
+    if (!this.__GRID_NUMBERS_MODULE__showNumbers) return;
     const textContainer = new PIXI.Container({ name: "grid-numbers" });
     const { size, width, height } = this.grid.options.dimensions;
     const textStyle = CONFIG?.canvasTextStyle ?? FALLBACK_TEXT_STYLE;
     textStyle.fontSize = size / 4;
-
-    let rows = Math.ceil(height / size);
-    let cols = Math.ceil(width / size) + 1;
-    for(let i = 1; i <= rows * cols; i++) {
-      const position_y = Math.ceil(i / cols);
-      const position_x = i - cols * Math.floor(i / cols);
-      let text = new PIXI.Text(i,textStyle);
-      const xOffset = text.width
-      const yOffset = size
-      text.x = position_x * size - xOffset;
-      text.y = position_y * size - yOffset;
-      textContainer.addChild(text)
+  
+    const rows = Math.ceil(height / size);
+    const cols = Math.ceil(width / size);
+    for (let y = 0; y <= rows; y++) {
+      for (let x = 0; x <= cols; x++) {
+        const orderNumber = cols * y + x + 1;
+        const text = new PIXI.Text(orderNumber, textStyle);
+        text.x = x * size + size - text.width;
+        text.y = y * size;
+        textContainer.addChild(text)
+      }
     }
     this.addChild(textContainer);
     Hooks.once('canvasReady', () => {
@@ -49,16 +46,21 @@ GridLayer.prototype.draw = async function() {
         textContainer.cacheAsBitmap = true;
       })
     })
-    
+  
+  }
 }
 
-function main() {
-  // CONFIG.debug.hooks = true
-}
+Hooks.on("init", function () {
+  patch();
+  game.keybindings.register("grid-numbers", "showGridNumbers", {
+    name: "Toggle grid numbers",
+    hint: "Shows a number of each cell on a grid",
+    editable: [{ key: "KeyN"}],
+    onDown: () => { 
+      canvas.grid.__GRID_NUMBERS_MODULE__toggleShowNumbers?.()
+    },
+    restricted: false,
+    precedence: CONST.KEYBINDING_PRECEDENCE.NORMAL
+  })
+})
 
-Hooks.on("ready", function(data) {
-  main()
-});
-
-// Hooks.on('renderApplication', data => {
-// })
